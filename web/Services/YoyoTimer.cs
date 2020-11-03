37771b68be0bb4298e622225e15259fb13860158
unrealnerd <arjunshetty2020@gmail.com>
2020-11-03 15:25:44 +0530

    * first one with decent start

---
+++ web/Services/YoyoTimer.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Timers;
+
+namespace web.Services
+{
+    public class YoyoTimer
+    {
+        private Timer _timer;
+        public event Action OnElapsed;
+
+        public void SetTimer(double intervalMilliseconds, bool repeat)
+        {
+            _timer = new Timer(intervalMilliseconds);
+            _timer.Elapsed += NotifyTimerElapsed;
+            _timer.AutoReset = repeat;
+            _timer.Enabled = true;
+        }
+
+
+        private void NotifyTimerElapsed(object source, ElapsedEventArgs e)
+        {
+            OnElapsed?.Invoke();
+            if (!_timer.AutoReset)
+            {
+                _timer.Stop();
+                _timer.Dispose();
+                _timer = null;
+            }
+        }
+
+    }
+}
